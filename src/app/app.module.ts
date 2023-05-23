97b1460eb00723e8bb74b83b2cf73ed13bd72f3a
simon <simon.miraglia@hotmail.fr>
2023-05-23 12:33:50 +0200

    changement pour safae

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HeaderComponent } from './header/header';
+import { HeaderComponent } from './common/header/header';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer';
+import { FooterComponent } from './common/footer/footer';
 import { AppRoutingModule } from './app-routing.module';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
