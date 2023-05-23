a14facd35752658ecf2568792ae08fa3eb542081
sarah <sarah.bousquet05@gmail.com>
2023-05-23 11:01:19 +0200

    changements pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HeaderComponent } from './header/header';
+import { HeaderComponent } from './common/header/header';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer';
+import { FooterComponent } from './common/footer/footer';
+import {BodyComponent} from './body/body';
+import { AppRoutingModule } from './app-routing.module';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
