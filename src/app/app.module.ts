5ad438cfec972254f4072f332e2bf83e0a1084b6
simon <simon.miraglia@hotmail.fr>
2023-05-23 11:43:04 +0200

    restructuration du site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer';
+import { AppRoutingModule } from './app-routing.module';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
