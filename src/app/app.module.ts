8b503fb108c338f6c4185d2967bb52ca2fa1bba5
sarah <sarah.bousquet05@gmail.com>
2023-05-24 15:02:29 +0200

    version sarah qui marche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import { Page4Component } from './page4/page4.component';
 import { Page5Component } from './page5/page5.component';
+import { MatTreeModule } from '@angular/material/tree';
 
 
 
@@ -34,9 +35,11 @@ import { Page5Component } from './page5/page5.component';
     MatDividerModule,
     MatButtonModule,
     AppRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
